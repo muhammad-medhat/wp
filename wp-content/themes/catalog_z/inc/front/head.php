e5e058c610869180d104f38e2719461333b7aa3e
muhammad medhat <muhammad.medhat@gmail.com>
2024-01-12 09:25:01 +0200

    Removing all files in .gitignore

--- wp-content/themes/catalog_z/inc/front/head.php
+++
@@ -1,8 +0,0 @@
-<?php
-function ud_head()
-{
-?>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<?php
-}
